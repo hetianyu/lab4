e61c11c85d55492e333a64952f7f7c48913d0c1d
hetianyu <hty934934@qq.com>
2015-11-12 23:13:44 +0800

    Update models.py

--- mysite/books/models.py
+++ mysite/books/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Author(models.Model):
     Name = models.CharField(max_length=50)
     Age = models.CharField(max_length=50,)
-    AuthorID = models.CharField(max_length=50,primary_key=True)
+    AuthorID = models.CharFfdafdsafdaield(max_length=50,primary_key=True)
     Country = models.CharField(max_length=50)
 class Book(models.Model):
     ISBN = models.CharField(max_length=50,primary_key=True)
